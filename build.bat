472a1f4a5f792705ee1cd1a8e844c6377da285f7
Robledo Pazotto <robledo@gmail.com>
2025-07-27 19:05:26 -0300

    day 4

--- build.bat
+++ build.bat
@@ -1,6 +1,6 @@
 @echo off
 
-REM The simple build script used by Casey
+REM A simple build script similar to the one used by Casey
 
 call "C:\Program Files\Microsoft Visual Studio\2022\Enterprise\VC\Auxiliary\Build\vcvarsall.bat" x64
 set path=Y:\handmade;%path%;
@@ -8,5 +8,5 @@ set path=Y:\handmade;%path%;
 mkdir build 2> nul
 pushd build
 
-cl -Zi -std:c++20 ..\src\win32_handmade.cpp user32.lib gdi32.lib
+cl -Zi -FC -std:c++20 ..\src\win32_handmade.cpp user32.lib gdi32.lib
 popd
--- src/win32_handmade.cpp
+++ src/win32_handmade.cpp
@@ -1,66 +1,86 @@
 #include <string>
 #include <windows.h>
+#include <stdint.h>
 
 // TODO: this is global for now
 static bool Running;
-static BITMAPINFO BitmapInfo; // BitmapInfo will be used to create a DIB section
-static void* BitmapMemory; // BitmapMemory will hold the pixel data for the DIB section
-static HBITMAP BitmapHandle; // BitmapHandle will be used to manage the DIB section
-static HDC BitmapDeviceContext; // BitmapDeviceContext will be used to draw the DIB section
+static BITMAPINFO BitmapInfo;
+static void* BitmapMemory;
+static int BitmapWidth;
+static int BitmapHeight;
+static int BytesPerPixel = 4;
+
+static void RenderWeirdGradient(int xOffset, int yOffset)
+{
+    int pitch = BitmapWidth * BytesPerPixel; // the number of bytes in a row of the bitmap
+    auto row = static_cast<uint8_t*>(BitmapMemory);
+
+    for (int y = 0; y < BitmapHeight; ++y)
+    {
+        auto pixel = reinterpret_cast<uint32_t*>(row);
+        for (int x = 0; x < BitmapWidth; ++x)
+        {
+            uint8_t blue = x + xOffset;
+            uint8_t green = y + yOffset;
+
+            *pixel++ = green << 8 | blue;
+        }
+
+        row += pitch;
+    }
+}
 
 // This function resizes the DIB section to the specified width and height
 static void Win32ResizeDIBSection(const int width, const int height)
 {
-    // TODO: bulletproof this
-    // maybe don't free first, free after, then free first if that fails
-
-    if (BitmapHandle)
+    if (BitmapMemory)
     {
-        // DeleteObject will delete the bitmap handle and free the memory associated with it
-        DeleteObject(BitmapHandle);
+        VirtualFree(BitmapMemory, 0, MEM_RELEASE);
     }
 
-    if (!BitmapDeviceContext)
-    {
-        // TODO: should we recreate these under certain special circumstances
-        BitmapDeviceContext = CreateCompatibleDC(nullptr);
-    }
+    BitmapWidth = width;
+    BitmapHeight = height;
 
     BitmapInfo.bmiHeader.biSize = sizeof(BitmapInfo.bmiHeader); // Size of the BITMAPINFOHEADER structure
-    BitmapInfo.bmiHeader.biWidth = width;
-    BitmapInfo.bmiHeader.biHeight = height;
+    BitmapInfo.bmiHeader.biWidth = BitmapWidth;
+    BitmapInfo.bmiHeader.biHeight = -BitmapHeight; // Negative height indicates a top-down bitmap
     BitmapInfo.bmiHeader.biPlanes = 1; // Number of color planes must be 1
     BitmapInfo.bmiHeader.biBitCount = 32; // 32 bits per pixel (RGBA)
     BitmapInfo.bmiHeader.biCompression = BI_RGB;
 
-    // A DIB section is a bitmap that can be used to draw directly to the screen.
-    BitmapHandle = CreateDIBSection(BitmapDeviceContext,
-                                    &BitmapInfo,
-                                    DIB_RGB_COLORS,
-                                    &BitmapMemory,
-                                    nullptr,
-                                    0);
+    int bitmapMemorySize = BitmapWidth * BitmapHeight * BytesPerPixel;
+
+    // Allocate memory for the bitmap with read/write permissions
+    BitmapMemory = VirtualAlloc(
+        nullptr,
+        bitmapMemorySize,
+        MEM_COMMIT,
+        PAGE_READWRITE);
 }
 
 // Updates the window with the pixel data from BitmapMemory
 static void Win32UpdateWindow(
     HDC deviceContext,
+    const RECT* windowRect,
     const int x,
     const int y,
     const int width,
     const int height)
 {
+    int windowWidth = windowRect->right - windowRect->left;
+    int windowHeight = windowRect->bottom - windowRect->top;
+
     // StretchDIBits will copy the pixel data from BitmapMemory to the device context.
     // It will stretch the pixels to fit the specified width and height.
     StretchDIBits(deviceContext,
-                  x,
-                  y,
-                  width,
-                  height,
-                  x,
-                  y,
-                  width,
-                  height,
+                  0,
+                  0,
+                  BitmapWidth,
+                  BitmapHeight,
+                  0,
+                  0,
+                  BitmapWidth,
+                  BitmapHeight,
                   BitmapMemory,
                   &BitmapInfo,
                   DIB_RGB_COLORS,
@@ -128,7 +148,10 @@ LRESULT CALLBACK Win32MainWindowCallback(
         const int height = paint.rcPaint.bottom - paint.rcPaint.top;
         const int width = paint.rcPaint.right - paint.rcPaint.left;
 
-        Win32UpdateWindow(deviceContext, x, y, width, height);
+        RECT clientRect;
+        GetClientRect(window, &clientRect);
+
+        Win32UpdateWindow(deviceContext, &clientRect, x, y, width, height);
 
         EndPaint(window, &paint);
     }
@@ -175,20 +198,21 @@ int CALLBACK WinMain(HINSTANCE hInstance,
 
         if (windowHandle)
         {
+            int xOffset = 0;
+            int yOffset = 0;
             // Start handling messages
             Running = true;
             while (Running)
             {
                 // Pull a message from the message queue
                 MSG message;
-                BOOL messageResult = GetMessageA(
-                    &message,
-                    nullptr,
-                    0,
-                    0);
-
-                if (messageResult > 0) // 0 is WM_QUIT, -1 is an error
+                while (PeekMessageA(&message, nullptr, 0, 0, PM_REMOVE))
                 {
+                    if (message.message == WM_QUIT)
+                    {
+                        Running = false;
+                    }
+
                     // translates virtual key messages into character messages
                     TranslateMessage(&message);
 
@@ -197,10 +221,19 @@ int CALLBACK WinMain(HINSTANCE hInstance,
                     // Windows may send messages to the window without going through this (without putting it in the queue).
                     DispatchMessageA(&message);
                 }
-                else
-                {
-                    break;
-                }
+                RenderWeirdGradient(xOffset, yOffset);
+
+                HDC deviceContext = GetDC(windowHandle);
+                RECT clientRect;
+                GetClientRect(windowHandle, &clientRect);
+                int windowWidth = clientRect.right - clientRect.left;
+                int windowHeight = clientRect.bottom - clientRect.top;
+                Win32UpdateWindow(deviceContext, &clientRect, 0, 0, windowWidth, windowHeight);
+
+                ReleaseDC(windowHandle, deviceContext);
+
+                ++xOffset;
+                ++yOffset;
             }
         }
         else
