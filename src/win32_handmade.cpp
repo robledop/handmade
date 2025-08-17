482bc52930989faec0c6feaf74955f4352a4dd1d
Robledo Pazotto <robledo@gmail.com>
2025-08-17 18:59:28 -0300

    day 6

--- src/win32_handmade.cpp
+++ src/win32_handmade.cpp
@@ -1,8 +1,10 @@
 #include <cstdint>
 #include <string>
 #include <windows.h>
+#include <xinput.h>
 
 struct win32_offscreen_buffer {
+    // NOTE: Pixels are always 32-bits wide, Memory Order 0x BB GG RR xx
     BITMAPINFO Info;
     void* Memory;
     int Width;
@@ -15,6 +17,39 @@ struct win32_window_dimensions {
     int Width;
     int Height;
 };
+
+// NOTE: We are declaring these functions ourselves because we don't want to link with xinput directly
+#define X_INPUT_GET_STATE(name) DWORD WINAPI name([[maybe_unused]] DWORD dwUserIndex, [[maybe_unused]] XINPUT_STATE* pState)
+typedef X_INPUT_GET_STATE(x_input_get_state);
+X_INPUT_GET_STATE(XInputGetStateStub)
+{
+    return 0;
+}
+
+// NOTE: If for some reason we are unable to load XInput, we will use this stub function that does nothing
+static x_input_get_state* XInputGetState_ = XInputGetStateStub;
+#define XInputGetState XInputGetState_
+
+#define X_INPUT_SET_STATE(name) DWORD WINAPI name([[maybe_unused]] DWORD dwUserIndex,[[maybe_unused]] XINPUT_VIBRATION* pVibration)
+typedef X_INPUT_SET_STATE(x_input_set_state);
+X_INPUT_SET_STATE(XInputSetStateStub)
+{
+    return 0;
+}
+
+// NOTE: If for some reason we are unable to load XInput, we will use this stub function that does nothing
+static x_input_set_state* XInputSetState_ = XInputSetStateStub;
+#define XInputSetState XInputSetState_
+
+static void Win32LoadXInput()
+{
+    // NOTE: If we are able to load XInput, replace the stubs with the real functions
+    if (HMODULE x_input_library = LoadLibraryA("xinput1_4.dll"))
+    {
+        XInputGetState_ = reinterpret_cast<x_input_get_state*>(GetProcAddress(x_input_library, "XInputGetState"));
+        XInputSetState_ = reinterpret_cast<x_input_set_state*>(GetProcAddress(x_input_library, "XInputSetState"));
+    }
+}
 
 static bool GlobalRunning;
 static win32_offscreen_buffer GlobalBackBuffer;
@@ -113,13 +148,6 @@ LRESULT CALLBACK Win32MainWindowCallback(
     LRESULT result = 0;
     switch (message)
     {
-    case WM_KEYDOWN:
-    {
-        OutputDebugStringA(("WM_KEYDOWN " +
-            std::to_string(static_cast<int>(wParam)) + "\n").c_str());
-    }
-    break;
-
     case WM_DESTROY:
     {
         // TODO: handle this as an error - recreate window?
@@ -142,17 +170,163 @@ LRESULT CALLBACK Win32MainWindowCallback(
     }
     break;
 
+    case WM_SYSKEYUP:
+    case WM_SYSKEYDOWN:
+    case WM_KEYDOWN:
+    case WM_KEYUP:
+    {
+#define KeyMessageWasDownBit (1 << 30)
+#define KeyMessageWasUpBit (1 << 31)
+
+        uint32_t vk_code = wParam;
+
+        bool was_down = (lParam & KeyMessageWasDownBit) != 0;
+        bool is_down = (lParam & KeyMessageWasUpBit) == 0;
+
+        // Avoid key repeats
+        if (was_down == is_down)
+        {
+            break;
+        }
+
+        switch (vk_code)
+        {
+        case VK_ESCAPE:
+        {
+            GlobalRunning = false;
+            OutputDebugStringA("ESCAPE: ");
+            if (is_down)
+            {
+                OutputDebugStringA("IS DOWN ");
+            }
+
+            if (was_down)
+            {
+                OutputDebugStringA("WAS DOWN ");
+            }
+
+            OutputDebugStringA("\n");
+        }
+        break;
+
+        case 0x57: // W
+        {
+            OutputDebugStringA("W\n");
+        }
+        break;
+
+        case 0x41: // A
+        {
+            OutputDebugStringA("A\n");
+        }
+        break;
+
+        case 0x53: // S
+        {
+            OutputDebugStringA("S\n");
+        }
+        break;
+
+        case 0x44: // D
+        {
+            OutputDebugStringA("D\n");
+        }
+        break;
+
+        case 0x45: // E
+        {
+            OutputDebugStringA("E\n");
+        }
+        break;
+
+        case 0x46: // F
+        {
+            OutputDebugStringA("F\n");
+        }
+        break;
+
+        case 0x47: // G
+        {
+            OutputDebugStringA("G\n");
+        }
+        break;
+
+        case VK_SPACE: // Space
+        {
+            OutputDebugStringA("SPACE\n");
+        }
+        break;
+
+        case VK_RETURN: // Enter
+        {
+            OutputDebugStringA("ENTER\n");
+        }
+        break;
+
+        case VK_TAB: // Tab
+        {
+            OutputDebugStringA("TAB: ");
+            if (is_down)
+            {
+                OutputDebugStringA("IS DOWN\n");
+            }
+
+            if (was_down)
+            {
+                OutputDebugStringA("WAS DOWN\n");
+            }
+        }
+        break;
+
+        case VK_BACK: // Backspace
+        {
+            OutputDebugStringA("BACKSPACE\n");
+        }
+        break;
+
+        case VK_DELETE: // Delete
+        {
+            OutputDebugStringA("DELETE\n");
+        }
+        break;
+
+        case VK_LEFT: // Left arrow
+        {
+            OutputDebugStringA("LEFT\n");
+        }
+        break;
+
+        case VK_RIGHT: // Right arrow
+        {
+            OutputDebugStringA("RIGHT\n");
+        }
+        break;
+
+        case VK_UP: // Up arrow
+        {
+            OutputDebugStringA("UP\n");
+        }
+        break;
+
+        case VK_DOWN: // Down arrow
+        {
+            OutputDebugStringA("DOWN\n");
+        }
+        break;
+
+        default:
+            // Unhandled key
+            break;
+        }
+    }
+    break;
+
     case WM_PAINT: // Window needs to be repainted
     {
         OutputDebugStringA("WM_PAINT\n");
         PAINTSTRUCT paint;
         // Prepare the window for painting and returns a device context
         HDC deviceContext = BeginPaint(window, &paint);
-
-        const int x = paint.rcPaint.left;
-        const int y = paint.rcPaint.top;
-        const int height = paint.rcPaint.bottom - paint.rcPaint.top;
-        const int width = paint.rcPaint.right - paint.rcPaint.left;
 
         auto [Width, Height] = Win32GetWindowDimensions(window);
         Win32DisplayBufferInWindow(deviceContext,
@@ -178,7 +352,8 @@ int CALLBACK WinMain(HINSTANCE hInstance,
     [[maybe_unused]] LPSTR lpCmdLine,
     [[maybe_unused]] int nShowCmd)
 {
-    WNDCLASS WindowClass = { };
+    Win32LoadXInput();
+    WNDCLASSA WindowClass = { };
 
     Win32ResizeDIBSection(&GlobalBackBuffer, 1280, 720);
     OutputDebugStringA("WM_SIZE\n");
@@ -237,19 +412,67 @@ int CALLBACK WinMain(HINSTANCE hInstance,
                     // Windows may send messages to the window without going through this (without putting it in the queue).
                     DispatchMessageA(&message);
                 }
+
+                XINPUT_VIBRATION vibration;
+                // TODO: Should we poll this more frequently?
+                for (DWORD controller_index = 0; controller_index < XUSER_MAX_COUNT; ++controller_index)
+                {
+                    XINPUT_STATE controller_state;
+                    if (XInputGetState(controller_index, &controller_state) == ERROR_SUCCESS)
+                    {
+                        // NOTE: This controller is plugged in.
+                        XINPUT_GAMEPAD* pad = &controller_state.Gamepad;
+                        bool up = pad->wButtons & XINPUT_GAMEPAD_DPAD_UP;
+                        bool down = pad->wButtons & XINPUT_GAMEPAD_DPAD_DOWN;
+                        bool left = pad->wButtons & XINPUT_GAMEPAD_DPAD_LEFT;
+                        bool right = pad->wButtons & XINPUT_GAMEPAD_DPAD_RIGHT;
+                        bool start = pad->wButtons & XINPUT_GAMEPAD_START;
+                        bool back = pad->wButtons & XINPUT_GAMEPAD_BACK;
+                        bool left_shoulder = pad->wButtons & XINPUT_GAMEPAD_LEFT_SHOULDER;
+                        bool right_shoulder = pad->wButtons & XINPUT_GAMEPAD_RIGHT_SHOULDER;
+                        bool left_trigger = pad->bLeftTrigger > 0;
+                        bool right_trigger = pad->bRightTrigger > 0;
+                        bool a = pad->wButtons & XINPUT_GAMEPAD_A;
+                        bool b = pad->wButtons & XINPUT_GAMEPAD_B;
+                        bool x = pad->wButtons & XINPUT_GAMEPAD_X;
+                        bool y = pad->wButtons & XINPUT_GAMEPAD_Y;
+                        int16_t left_thumb_x = pad->sThumbLX;
+                        int16_t left_thumb_y = pad->sThumbLY;
+                        int16_t right_thumb_x = pad->sThumbRX;
+                        int16_t right_thumb_y = pad->sThumbRY;
+
+                        xOffset += left_thumb_x >> 14;
+                        yOffset += left_thumb_y >> 14;
+
+
+                        if (a)
+                        {
+                            OutputDebugStringA("A\n");
+                            yOffset += 2;
+
+                            vibration.wLeftMotorSpeed = 60000;
+                            vibration.wRightMotorSpeed = 60000;
+                            XInputSetState(0, &vibration);
+                        }
+                        else
+                        {
+                            vibration.wLeftMotorSpeed = 0;
+                            vibration.wRightMotorSpeed = 0;
+                            XInputSetState(0, &vibration);
+                        }
+                    }
+                    else
+                    {
+                        // NOTE: The controller is not available.
+                    }
+                }
+
                 RenderWeirdGradient(GlobalBackBuffer, xOffset, yOffset);
 
-                // RECT clientRect;
-                // GetClientRect(windowHandle, &clientRect);
-                // int windowWidth = clientRect.right - clientRect.left;
-                // int windowHeight = clientRect.bottom - clientRect.top;
                 win32_window_dimensions dimensions = Win32GetWindowDimensions(windowHandle);
                 Win32DisplayBufferInWindow(deviceContext, dimensions.Width, dimensions.Height, GlobalBackBuffer);
 
                 ReleaseDC(windowHandle, deviceContext);
-
-                ++xOffset;
-                ++yOffset;
             }
         }
         else
